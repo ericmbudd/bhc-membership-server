d9f72a233654d7472dfbff35a7e68d752750e31e
Eric Budd <ericbudd@gmail.com>
2018-07-19 14:11:13 -0600

    add more sample applications

--- seeds/002_applications.js
+++ seeds/002_applications.js
@@ -50,7 +50,80 @@ exports.seed = function( knex, Promise ) {
             "do you like Masala co-op?": "Yes"
           }
         }
+      },
+      {
+        id: 3,
+        contacts_id: 5,
+        active: true,
+        move_in_date: '2018-08-01T00:00:00Z',
+        current_city: "Superior",
+        current_state: "CO",
+        student: "No",
+        children: 0,
+        pronouns: "he, him, his",
+        wait_list: true,
+        vehicle: true,
+        bhc_questions: {
+          "do you like co-ops?": "Yes"
+        },
+        house_questions: {
+          1: {
+            "do you like Ingram co-op?": "Yes"
+          },
+          2: {
+            "do you like Masala co-op?": "Yes"
+          }
+        }
+      },
+      {
+        id: 4,
+        contacts_id: 5,
+        active: true,
+        move_in_date: '2018-08-01T00:00:00Z',
+        current_city: "Superior",
+        current_state: "CO",
+        student: "No",
+        children: 0,
+        pronouns: "he, him, his",
+        wait_list: true,
+        vehicle: true,
+        bhc_questions: {
+          "do you like co-ops?": "Yes"
+        },
+        house_questions: {
+          1: {
+            "do you like Ingram co-op?": "Yes"
+          },
+          2: {
+            "do you like Masala co-op?": "Yes"
+          }
+        }
+      },
+      {
+        id: 5,
+        contacts_id: 5,
+        active: true,
+        move_in_date: '2018-08-01T00:00:00Z',
+        current_city: "Superior",
+        current_state: "CO",
+        student: "No",
+        children: 0,
+        pronouns: "he, him, his",
+        wait_list: true,
+        vehicle: true,
+        bhc_questions: {
+          "do you like co-ops?": "Yes"
+        },
+        house_questions: {
+          1: {
+            "do you like Ingram co-op?": "Yes"
+          },
+          2: {
+            "do you like Masala co-op?": "Yes"
+          }
+        }
       }
+
     ] );
   } );
 };
